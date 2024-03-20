52e219744a4557af56d24be3d5e17c0b6d9bb119
Johnson adeola <johnsamxy@gmail.com>
2024-03-20 22:36:02 +0100

    new task

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- creates a table user with predefined columns
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
