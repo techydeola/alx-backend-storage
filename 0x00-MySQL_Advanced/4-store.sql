102e3a758bd246dcdcfb452dbda3127291db4d43
Johnson adeola <johnsamxy@gmail.com>
2024-03-21 14:12:02 +0100

    new task

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,15 @@
+-- a SQL script that creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+DELIMITER //
+
+CREATE TRIGGER decrease_item_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE NEW.item_name = items.name;
+END;
+//
+
+DELIMITER ;
