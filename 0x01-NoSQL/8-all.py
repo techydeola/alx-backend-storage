31be0da70bb1702fa26ccf9052ecd7f29726d224
Johnson adeola <johnsamxy@gmail.com>
2024-03-25 22:46:53 +0100

    new task

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    a Python module for task 8 of NoSQL project
+"""
+
+
+def list_all(mongo_collection):
+    """
+        a function that lists all documents in a collection
+        Return: a list of documents
+    """
+    coll_list = []
+    for obj in mongo_collection.find():
+        coll_list.append(obj)
+    
+    return coll_list
