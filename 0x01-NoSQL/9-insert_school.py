ede3f3b4d6272a99ca816ddd068053b08811f1ea
Johnson adeola <johnsamxy@gmail.com>
2024-03-26 01:11:57 +0100

    new task

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    a Python module for task 9 of NoSQL project
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+        a function that inserts a new document in a collection
+        Return: nothing
+    """
+    data = {key: value for key, value in kwargs.items()}
+    mongo_collection.insert_one(data)
